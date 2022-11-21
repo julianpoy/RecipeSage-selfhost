2013433a32866907d24d82b0b15d90aeccb6541d
Julian Poyourow <julian@poyourow.com>
2022-11-21 02:19:46 +0000

    Fix changing static base later

--- static.entrypoint.sh
+++ static.entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sed -i 's|<base href="\/">$|<base href="/"><script>window.API_BASE_OVERRIDE = '${API_BASE_OVERRIDE}';</script>|i' /usr/share/nginx/html/index.html
+sed -i 's|<base href="\/">.*|<base href="/"><script>window.API_BASE_OVERRIDE = '${API_BASE_OVERRIDE}';</script>|i' /usr/share/nginx/html/index.html
 
