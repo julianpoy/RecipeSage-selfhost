3651718a7bfef12464a48a406e89e7ac9e8878a7
Evs91 <edamelio@gmail.com>
2023-08-19 10:54:50 -0400

    Update migrate.sh for compose v2
    Docker Compose V1 is sunset, Compose V2 command is now "docker compose" instead of "docker-compose"

--- migrate.sh
+++ migrate.sh
@@ -2,7 +2,7 @@
 
 echo "Performing migrations. This could take up to 1 minute."
 
-docker-compose exec api tsx packages/backend/src/migrate
+docker compose exec api tsx packages/backend/src/migrate
 
 echo "Migrations complete."
 
